eb4e606123ca1c307985e6cce4553dcc1bec3d0f
Lincoln Baxter <lincolnbaxter@gmail.com>
2010-09-14 13:11:13 +0000

    Downgraded weldx version for compatibility with Faces - eventually this needs to be re-upgraded, but there are blocking API differences.

--- impl/src/main/java/org/jboss/seam/servlet/event/AbstractServletEventBridge.java
+++ impl/src/main/java/org/jboss/seam/servlet/event/AbstractServletEventBridge.java
@@ -3,6 +3,8 @@ package org.jboss.seam.servlet.event;
 import java.lang.annotation.Annotation;
 import java.util.Arrays;
 
+import javax.enterprise.inject.spi.BeanManager;
+
 import org.jboss.weld.extensions.beanManager.BeanManagerAware;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,19 +16,30 @@ public class AbstractServletEventBridge extends BeanManagerAware
 
    protected void fireEvent(final Object payload, final Annotation... qualifiers)
    {
-      if (isBeanManagerAvailable())
+      // TODO - this should probably be replaced with an isBeanManagerAvailable() method.
+      BeanManager beanManager;
+      try
+      {
+         beanManager = getBeanManager();
+      }
+      catch (Exception e)
+      {
+         beanManager = null;
+      }
+
+      if (beanManager != null)
       {
          /*
-          * We can't always guarantee the BeanManager will be available,
-          * especially in environemnts where we don't control bootstrap order
-          * that well (e.g. Servlet containers)
+          * We can't always guarantee the BeanManager will be available, especially in environments where we don't
+          * control bootstrap order that well (e.g. Servlet containers)
           */
          log.trace("Firing event #0 with qualifiers #1", payload, Arrays.asList(qualifiers));
-         getBeanManager().fireEvent(payload, qualifiers);
+         beanManager.fireEvent(payload, qualifiers);
       }
       else
       {
-         log.debug("BeanManager can't be found so not sending event " + payload + " with qualifiers " + Arrays.asList(qualifiers));
+         log.debug("BeanManager can't be found so not sending event " + payload + " with qualifiers "
+                  + Arrays.asList(qualifiers));
       }
    }
 
