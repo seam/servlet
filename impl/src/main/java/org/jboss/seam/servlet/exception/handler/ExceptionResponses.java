030a064abad4c79ea05d69576208369e83a3b248
Shane Bryzak <sbryzak@gmail.com>
2011-01-26 12:23:39 +1000

    update parent and bom versions, fix compiler error

--- impl/src/main/java/org/jboss/seam/servlet/exception/handler/ExceptionResponses.java
+++ impl/src/main/java/org/jboss/seam/servlet/exception/handler/ExceptionResponses.java
@@ -5,11 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.jboss.seam.solder.servicehandler.ServiceHandler;
+import org.jboss.seam.solder.serviceHandler.ServiceHandlerType;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@ServiceHandler(ExceptionResponseServiceHandler.class)
+@ServiceHandlerType(ExceptionResponseServiceHandler.class)
 public @interface ExceptionResponses
 {
 }
